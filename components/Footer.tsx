b7484d1ef164bcff8cbad057e96cdc3cf65a7b11
CubingSoda <82660497+CubingSoda@users.noreply.github.com>
2022-10-05 10:58:25 -0500

    fix footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import styles from "styles/components/Footer.module.scss";
 
 const Footer = () => {
-  const app = useContext(AppContext);
   return (
     <footer className={styles.footer}>
       <div className={styles.footer1}>
