7247295eb27aac7f336868f2411e70a729ddbac1
CubingSoda <82660497+CubingSoda@users.noreply.github.com>
2022-10-05 16:14:11 -0500

    fix deployment (4)

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -55,7 +55,7 @@ const Post: React.FC<PostProps> = ({ postData, content }) => {
             return (
               <span
                 className={`${styles.tag} ${
-                  typeof tag === "string"
+                  typeof tag.toLowerCase() === "string"
                     ? app.selectedTags.includes(
                         tag.toLowerCase().replaceAll(" ", "")
                       )
