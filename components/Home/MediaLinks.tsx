f042755b0f47d7cce62f0a521ae3d99dd8544f6a
CubingSoda <82660497+CubingSoda@users.noreply.github.com>
2022-10-11 23:44:51 -0500

    better media links

---
+++ components/Home/MediaLinks.tsx
@@ -0,0 +1,89 @@
+import styles from "styles/Home.module.scss";
+
+interface MediaLinksProps {
+  toggle: boolean;
+  open: () => void;
+}
+
+const MediaLinks: React.FC<MediaLinksProps> = ({ toggle, open }) => {
+  const links = [
+    {
+      name: "GitHub",
+      img: "/img/icons/github.png",
+      href: "https://github.com/CubingSoda",
+    },
+    {
+      name: "Reddit",
+      img: "/img/icons/reddit.png",
+      href: "https://www.reddit.com/user/CubingSoda/",
+    },
+    {
+      name: "Discord",
+      img: "/img/icons/discord.png",
+      href: "https://discord.com/users/822191907286417450",
+    },
+    {
+      name: "Monkeytype",
+      img: "/img/icons/monkeytype.png",
+      href: "https://monkeytype.com/profile/cubingsoda",
+    },
+    {
+      name: "Stackoverflow",
+      img: "/img/icons/stackoverflow.png",
+      href: "https://stackoverflow.com/users/17556679/cubingsoda",
+    },
+    {
+      name: "NPM",
+      img: "/img/icons/npm.png",
+      href: "https://www.npmjs.com/~cubingsoda",
+    },
+  ];
+
+  const LinksComponent = () => {
+    return (
+      <>
+        {links.map((link) => {
+          return (
+            <a
+              href={link.href}
+              rel="noreferrer"
+              target="_blank"
+              className={link.name}
+            >
+              <img
+                src={link.img}
+                alt={`${link.name}`}
+                className={styles.logo}
+              />
+            </a>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <div className={styles.link}>
+      {/* Desktop */}
+      <section className={styles.mobile} onClick={open}>
+        {toggle ? (
+          <i className="fa-solid fa-arrow-down fa-2xl"></i>
+        ) : (
+          <i className="fa-solid fa-arrow-up fa-2xl"></i>
+        )}
+        <span>{toggle ? <>Hide</> : <>Show</>} Social Profiles</span>
+
+        <div className="popup-text">
+          <LinksComponent />
+        </div>
+      </section>
+
+      {/* Mobile */}
+      <section className={styles.desktop}>
+        <LinksComponent />
+      </section>
+    </div>
+  );
+};
+
+export default MediaLinks;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import UI from "components/UI";
-import MediaLink from "components/Home/MediaLink";
-
 import { useState } from "react";
 import Link from "next/link";
+
+import UI from "components/UI";
+import MediaLinks from "components/Home/MediaLinks";
 
 import styles from "styles/Home.module.scss";
 
@@ -51,82 +51,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className={styles.link}>
-            <section className={styles.mobile} onClick={open}>
-              {toggle ? (
-                <i className="fa-solid fa-arrow-down fa-2xl"></i>
-              ) : (
-                <i className="fa-solid fa-arrow-up fa-2xl"></i>
-              )}
-              <span>{toggle ? <>Hide</> : <>Show</>} Social Profiles</span>
-
-              <div className="popup-text">
-                <MediaLink
-                  name="GitHub"
-                  img="/img/icons/github.png"
-                  href="https://github.com/CubingSoda"
-                />
-                <MediaLink
-                  name="Reddit"
-                  img="/img/icons/reddit.png"
-                  href="https://www.reddit.com/user/CubingSoda/"
-                />
-                <MediaLink
-                  name="Discord"
-                  img="/img/icons/discord.png"
-                  href="https://discord.com/users/822191907286417450"
-                />
-                <MediaLink
-                  name="Stackoverflow"
-                  img="/img/icons/stackoverflow.png"
-                  href="https://stackoverflow.com/users/17556679/cubingsoda"
-                />
-                <MediaLink
-                  name="10FastFingers"
-                  img="/img/icons/10fastfingers.png"
-                  href="https://10fastfingers.com/user/3212833/"
-                />
-                <MediaLink
-                  name="Replit"
-                  img="/img/icons/replit.png"
-                  href="https://replit.com/@cubingsoda"
-                />
-              </div>
-            </section>
-
-            <section className={styles.desktop}>
-              <MediaLink
-                name="GitHub"
-                img="/img/icons/github.png"
-                href="https://github.com/CubingSoda"
-              />
-              <MediaLink
-                name="Reddit"
-                img="/img/icons/reddit.png"
-                href="https://www.reddit.com/user/CubingSoda/"
-              />
-              <MediaLink
-                name="Discord"
-                img="/img/icons/discord.png"
-                href="https://discord.com/users/822191907286417450"
-              />
-              <MediaLink
-                name="Stackoverflow"
-                img="/img/icons/stackoverflow.png"
-                href="https://stackoverflow.com/users/17556679/cubingsoda"
-              />
-              <MediaLink
-                name="10FastFingers"
-                img="/img/icons/10fastfingers.png"
-                href="https://10fastfingers.com/user/3212833/"
-              />
-              <MediaLink
-                name="Replit"
-                img="/img/icons/replit.png"
-                href="https://replit.com/@cubingsoda"
-              />
-            </section>
-          </div>
+          <MediaLinks toggle={toggle} open={open} />
         </div>
       </main>
     </UI>
